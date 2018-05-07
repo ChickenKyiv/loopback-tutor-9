c4dadfcff19c11ea386ab5c7a29e4667f21726e5
Louise van den Berg <louisevdb84@gmail.com>
2018-05-07 12:39:20 +0200

    comments regarding relations

--- bin/reservation.js
+++ bin/reservation.js
@@ -5,7 +5,9 @@ const get = () => {
      var reservation    = [
         {        
         "startDate": "2017-03-21",
-        "endDate": "2017-03-23",
+             "endDate": "2017-03-23",
+        //Here campground and customer is referenced in reservation
+        //Is this enough to bind it in our import script?
         "campgroundId": 1,
         "customerId": 2 
          },       
